5166ccdaeeb021d9a98a4b8b0a9e7679689642bc
Tomáš Cupal <Tomas.Cupal@cleverlance.com>
2015-05-06 17:56:30 +0200

    Refactoring.

--- Masters/Assets/Scripts/LockCameraToBoxCollider2D.cs
+++ Masters/Assets/Scripts/LockCameraToBoxCollider2D.cs
@@ -1,33 +1,36 @@
-﻿using Assets.Scripts;
+﻿using Assets.Shared.Scripts;
 using UnityEngine;
 
-[RequireComponent(typeof (Camera))]
-public class LockCameraToBoxCollider2D : MonoBehaviour
+namespace Assets.Scripts
 {
-    public BoxCollider2D rect;
-
-    private void LateUpdate()
+    [RequireComponent(typeof (Camera))]
+    public class LockCameraToBoxCollider2D : MonoBehaviour
     {
-        if (rect != null)
+        public BoxCollider2D Rect;
+
+        private void LateUpdate()
         {
-            // Extract infomation about camera rectangle from orthographic size.
-            float aspectRatio = (float) Screen.width/Screen.height;
-            float height = GetComponent<Camera>().orthographicSize;
-            float width = GetComponent<Camera>().orthographicSize*aspectRatio;
-            var cameraExtents = new Vector3(width, height, 0);
+            if (Rect != null)
+            {
+                // Extract infomation about camera rectangle from orthographic size.
+                var aspectRatio = (float) Screen.width/Screen.height;
+                var height = GetComponent<Camera>().orthographicSize;
+                var width = GetComponent<Camera>().orthographicSize*aspectRatio;
+                var cameraExtents = new Vector3(width, height, 0);
 
-            Rect cameraRect = toRect(transform.position, cameraExtents);
-            Rect colliderRect = toRect(rect.transform.position, rect.bounds.extents);
+                var cameraRect = toRect(transform.position, cameraExtents);
+                var colliderRect = toRect(Rect.transform.position, Rect.bounds.extents);
 
-            if (cameraRect.xMin < colliderRect.xMin) transform.SetX(colliderRect.xMin + cameraExtents.x);
-            if (cameraRect.xMax > colliderRect.xMax) transform.SetX(colliderRect.xMax - cameraExtents.x);
-            if (cameraRect.yMin < colliderRect.yMin) transform.SetY(colliderRect.yMin + cameraExtents.y);
-            if (cameraRect.yMax > colliderRect.yMax) transform.SetY(colliderRect.yMax - cameraExtents.y);
+                if (cameraRect.xMin < colliderRect.xMin) transform.SetX(colliderRect.xMin + cameraExtents.x);
+                if (cameraRect.xMax > colliderRect.xMax) transform.SetX(colliderRect.xMax - cameraExtents.x);
+                if (cameraRect.yMin < colliderRect.yMin) transform.SetY(colliderRect.yMin + cameraExtents.y);
+                if (cameraRect.yMax > colliderRect.yMax) transform.SetY(colliderRect.yMax - cameraExtents.y);
+            }
         }
-    }
 
-    private Rect toRect(Vector3 center, Vector3 extents)
-    {
-        return new Rect(center.x - extents.x, center.y - extents.y, extents.x*2, extents.y*2);
+        private Rect toRect(Vector3 center, Vector3 extents)
+        {
+            return new Rect(center.x - extents.x, center.y - extents.y, extents.x*2, extents.y*2);
+        }
     }
 }

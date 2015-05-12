1adbecb6ad2d1bd3b350af1d00cb1bc098781cbf
Tomáš Cupal <Tomas.Cupal@cleverlance.com>
2015-05-12 15:14:56 +0200

    Adding Jump action.

---
+++ Masters/Assets/Scripts/Avatar.cs
@@ -0,0 +1,62 @@
+﻿using Assets.Scripts;
+using Assets.Shared.Scripts;
+using UnityEngine;
+
+[RequireComponent(typeof (Animator), typeof (Tween))]
+public class Avatar : MonoBehaviour
+{
+    public KeyCode CrouchKeyCode;
+    public KeyCode JumpKeyCode;
+    public KeyCode PunchKeyCode;
+    public float jumpDuration = 1;
+    public float jumpHeight = 10;
+    public float speed = 15;
+    private Tween tween;
+
+    private void Start()
+    {
+        tween = GetComponent<Tween>();
+        tween.OnTween += OnTween;
+        tween.OnFinish += () => GetComponent<Animator>().SetFloat("Jump", 0);
+    }
+
+    private void Update()
+    {
+        var axis = Input.GetAxis("Horizontal");
+        transform.AddX(speed*axis*Time.deltaTime);
+        if (axis < 0)
+        {
+            transform.localScale = Vector3.one.Multiply(new Vector3(-1, 1, 1));
+        }
+        if (axis > 0)
+        {
+            transform.localScale = Vector3.one.Multiply(new Vector3(1, 1, 1));
+        }
+
+        if (Input.GetKeyDown(PunchKeyCode))
+        {
+            GetComponent<Animator>().SetTrigger("Punch");
+        }
+
+        if (Input.GetKeyDown(CrouchKeyCode))
+        {
+            GetComponent<Animator>().SetBool("Crouch", true);
+        }
+
+        if (Input.GetKeyUp(CrouchKeyCode))
+        {
+            GetComponent<Animator>().SetBool("Crouch", false);
+        }
+
+        if (Input.GetKeyUp(JumpKeyCode))
+        {
+            tween.startTween(jumpDuration);
+        }
+    }
+
+    private void OnTween(float progress, float tweenvalue)
+    {
+        transform.SetY(tweenvalue*jumpHeight);
+        GetComponent<Animator>().SetFloat("Jump", progress);
+    }
+}

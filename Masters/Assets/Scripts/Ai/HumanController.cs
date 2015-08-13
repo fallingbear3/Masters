ca19ee9732d31dc50f46904e9a03c4281d72bb3d
pixelthieves <pixelthievesgames@gmail.com>
2015-08-13 05:22:44 +0200

    Separating avatar controller from avatar class.

---
+++ Masters/Assets/Scripts/Ai/HumanController.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Shared.Scripts;
+using UnityEngine;
+
+namespace Assets.Scripts.Ai
+{
+    [RequireComponent(typeof(Avatar))]
+    public class HumanController : MonoBehaviour
+    {
+        public KeyCode CrouchKeyCode;
+        public KeyCode JumpKeyCode;
+        public KeyCode PunchKeyCode;
+        public KeyCode MusicWaveCode;
+
+        private void Update()
+        {
+            GetComponent<Avatar>().move( Input.GetAxis("Horizontal"));
+
+            if (Input.GetKeyDown(CrouchKeyCode))
+            {
+                GetComponent<Avatar>().crouch();
+            }
+            if (Input.GetKeyDown(JumpKeyCode))
+            {
+                GetComponent<Avatar>().jump();
+            }
+            if (Input.GetKeyDown(PunchKeyCode))
+            {
+                GetComponent<Avatar>().punch();
+            }
+            if (Input.GetKeyDown(MusicWaveCode))
+            {
+                GetComponent<Avatar>().musicWave();
+            }
+        }
+    }
+}

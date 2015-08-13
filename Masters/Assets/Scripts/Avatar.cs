cfe47f5a8ac9c1b09806fe0eaba6c91cd6f271fb
pixelthieves <pixelthievesgames@gmail.com>
2015-08-13 05:39:46 +0200

    Fixing merge issues.

--- Masters/Assets/Scripts/Avatar.cs
+++ Masters/Assets/Scripts/Avatar.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 [RequireComponent(typeof (Animator), typeof (Tween))]
 public class Avatar : MonoBehaviour
 {
-    public KeyCode CrouchKeyCode;
-    public KeyCode JumpKeyCode;
-    public KeyCode PunchKeyCode;
-    public KeyCode SpecialKeyCode;
     public float jumpDuration = 1;
     public float jumpHeight = 10;
     public float speed = 15;
@@ -22,48 +18,42 @@ public class Avatar : MonoBehaviour
         tween.OnFinish += () => GetComponent<Animator>().SetFloat("Jump", 0);
     }
 
-    private void Update()
+    public void musicWave()
     {
-        var axis = Input.GetAxis("Horizontal");
-        transform.AddX(speed*axis*Time.deltaTime);
-        if (axis < 0)
-        {
-            transform.localScale = Vector3.one.Multiply(new Vector3(-1, 1, 1));
-        }
-        if (axis > 0)
-        {
-            transform.localScale = Vector3.one.Multiply(new Vector3(1, 1, 1));
-        }
+        GetComponent<MusicWave>().execute();
+    }
 
-        if (Input.GetKeyDown(PunchKeyCode))
-        {
-            GetComponent<Animator>().SetTrigger("Punch");
-        }
+    public void jump()
+    {
+        tween.startTween(jumpDuration);
+    }
 
-        if (Input.GetKeyDown(CrouchKeyCode))
-        {
-            GetComponent<Animator>().SetBool("Crouch", true);
-        }
+    public void crouch()
+    {
+        GetComponent<Animator>().SetBool("Crouch", true);
+    }
 
-        if (Input.GetKeyUp(CrouchKeyCode))
-        {
-            GetComponent<Animator>().SetBool("Crouch", false);
-        }
-
-        if (Input.GetKeyUp(JumpKeyCode))
-        {
-            tween.startTween(jumpDuration);
-        }
-
-        if (Input.GetKeyUp(SpecialKeyCode))
-        {
-            GetComponent<MusicWave>().execute();
-        }
+    public void punch()
+    {
+        GetComponent<Animator>().SetTrigger("Punch");
     }
 
     private void OnTween(float progress, float tweenvalue)
     {
         transform.SetY(tweenvalue*jumpHeight);
         GetComponent<Animator>().SetFloat("Jump", progress);
+    }
+
+    public void move(float direction)
+    {
+        transform.AddX(speed * direction * Time.deltaTime);
+        if (direction < 0)
+        {
+            transform.localScale = Vector3.one.Multiply(new Vector3(-1, 1, 1));
+        }
+        if (direction > 0)
+        {
+            transform.localScale = Vector3.one.Multiply(new Vector3(1, 1, 1));
+        }
     }
 }
